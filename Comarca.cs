aa73f5421ded30775fb392a2f31540ae746078cb
argo <aleix.tuneu.7e7@itb.cat>
2024-04-18 12:15:09 +0200

    Add project files.

---
+++ Comarca.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+
+namespace AC4
+{
+    public class Comarca
+    {
+        public int Year { get; set; }
+        public int CodiComarca { get; set; }
+        public string NomComarca { get; set; }
+        public int Poblacio { get; set; }
+        public int DomesticXarxa { get; set; }
+        public int ActivitatsEconomiques { get; set; }
+        public int Total { get; set; }
+        public double ConsumDomesticPerCapita { get; set; }
+
+       
+
+        public Comarca(int Year, int codiComarca, string nomComarca, int poblacio, int domesticXarxa, int activitatsEconomiques, int total, double consumDomesticPerCapita)
+        {
+            Year = Year;
+            CodiComarca = codiComarca;
+            NomComarca = nomComarca;
+            Poblacio = poblacio;
+            DomesticXarxa = domesticXarxa;
+            ActivitatsEconomiques = activitatsEconomiques;
+            Total = total;
+            ConsumDomesticPerCapita = consumDomesticPerCapita;
+        }
+
+        public Comarca()
+        {
+        }
+    }
+}
---
+++ ComarcaDAO.cs
@@ -0,0 +1,115 @@
+﻿using Npgsql;
+
+using AC4;
+
+namespace dao.Persistence.Mapping
+{
+    public class ComarcaDAO : IComarcaDAO
+    {
+        private readonly string connectionString;
+
+   
+
+        public ComarcaDAO(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+
+        public ComarcaDTO GetComarcaById(int CodiComarca)
+        {
+            ComarcaDTO comarca = null;
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(NpgsqlUtils.OpenConnection()))
+            {
+                string query = "SELECT \"Year\", \"CodiComarca\", \"NomComarca\", \"Poblacio\", \"DomesticXarxa\", " +
+                               "\"ActivitatsEconomiques\", \"Total\", \"ConsumDomesticPerCapita\" " +
+                               "FROM \"Comarca\" WHERE \"CodiComarca\" = @CodiComarca";
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("@CodiComarca", CodiComarca);
+                connection.Open();
+                NpgsqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    // ORM: [--,--,--] -----> ContactDTO
+                    comarca = NpgsqlUtils.GetComarca(reader);
+                }
+            }
+
+            return comarca;
+        }
+
+        public void AddComarca(ComarcaDTO comarca)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                string query = "INSERT INTO \"Comarca\" (\"Year\", \"CodiComarca\", \"NomComarca\", \"Poblacio\", \"DomesticXarxa\", \"ActivitatsEconomiques\", \"Total\", \"ConsumDomesticPerCapita\") " +
+                               "VALUES (@Year, @CodiComarca, @NomComarca, @Poblacio, @DomesticXarxa, @ActivitatsEconomiques, @Total, @ConsumDomesticPerCapita)";
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Year", comarca.Year);
+                command.Parameters.AddWithValue("@CodiComarca", comarca.CodiComarca);
+                command.Parameters.AddWithValue("@NomComarca", comarca.NomComarca);
+                command.Parameters.AddWithValue("@Poblacio", comarca.Poblacio);
+                command.Parameters.AddWithValue("@DomesticXarxa", comarca.DomesticXarxa);
+                command.Parameters.AddWithValue("@ActivitatsEconomiques", comarca.ActivitatsEconomiques);
+                command.Parameters.AddWithValue("@Total", comarca.Total);
+                command.Parameters.AddWithValue("@ConsumDomesticPerCapita", comarca.ConsumDomesticPerCapita);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+        public void UpdateComarca(ComarcaDTO comarca)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                string query = "UPDATE \"Comarca\" SET \"NomComarca\" = @NomComarca, \"Poblacio\" = @Poblacio, " +
+                               "\"DomesticXarxa\" = @DomesticXarxa, \"ActivitatsEconomiques\" = @ActivitatsEconomiques, " +
+                               "\"Total\" = @Total, \"ConsumDomesticPerCapita\" = @ConsumDomesticPerCapita " +
+                               "WHERE \"Year\" = @Year AND \"CodiComarca\" = @CodiComarca"; NpgsqlCommand command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Year", comarca.Year);
+                command.Parameters.AddWithValue("@CodiComarca", comarca.CodiComarca);
+                command.Parameters.AddWithValue("@NomComarca", comarca.NomComarca);
+                command.Parameters.AddWithValue("@Poblacio", comarca.Poblacio);
+                command.Parameters.AddWithValue("@DomesticXarxa", comarca.DomesticXarxa);
+                command.Parameters.AddWithValue("@ActivitatsEconomiques", comarca.ActivitatsEconomiques);
+                command.Parameters.AddWithValue("@Total", comarca.Total);
+                command.Parameters.AddWithValue("@ConsumDomesticPerCapita", comarca.ConsumDomesticPerCapita);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteComarca(int CodiComarca)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                string query = "DELETE FROM \"Comarca\" WHERE \"CodiComarca\" = @CodiComarca";
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("@CodiComarca", CodiComarca);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public IEnumerable<ComarcaDTO> GetAllComarcas()
+        {
+            List<ComarcaDTO> comarcas = new List<ComarcaDTO>();
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(NpgsqlUtils.OpenConnection()))
+            {
+                string query = "SELECT \"Year\", \"CodiComarca\", \"NomComarca\", \"Poblacio\", \"DomesticXarxa\", " +
+                               "\"ActivitatsEconomiques\", \"Total\", \"ConsumDomesticPerCapita\" FROM \"Comarca\""; NpgsqlCommand command = new NpgsqlCommand(query, connection);
+                connection.Open();
+                NpgsqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    ComarcaDTO comarca = NpgsqlUtils.GetComarca(reader);
+                    comarcas.Add(comarca);
+                }
+            }
+            return comarcas;
+        }
+    }
+
+}
